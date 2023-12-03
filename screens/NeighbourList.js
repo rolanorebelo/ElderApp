392e56c6e84dc601fb1130ddaadb65191f07e7b5
Rolano <rolano.rebelo@gmail.com>
2023-12-03 09:13:57 -0600

    neighbour chat and task chat

--- screens/NeighbourList.js
+++ screens/NeighbourList.js
@@ -1,18 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-  Image,
-} from 'react-native';
-import { getFirestore, collection, getDocs,updateDoc, doc } from 'firebase/firestore';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image } from 'react-native';
+import { getFirestore, collection, getDocs, updateDoc, doc } from 'firebase/firestore';
 import { auth } from '../config/firebase';
 
 const NeighbourList = ({ navigation }) => {
   const [neighbors, setNeighbors] = useState([]);
-  
+
   useEffect(() => {
     fetchNeighbors();
   }, []);
@@ -20,84 +13,59 @@ const NeighbourList = ({ navigation }) => {
   const fetchNeighbors = async () => {
     try {
       const user = auth.currentUser;
-      console.log('Current User:', user);
-  
+
       const neighborsRef = collection(getFirestore(), 'users');
       const neighborsSnapshot = await getDocs(neighborsRef);
-  
+
       const neighborsData = [];
       neighborsSnapshot.forEach((doc) => {
         const neighborData = doc.data();
-        console.log('Neiggggg---------', neighborData);
-      
-        if (neighborData.uid !== user.uid) {
+        console.log('UsSSS------',user.uid);
+        console.log('Neiggggg---------', doc.id);
+        if (doc.id !== user.uid) {
           neighborsData.push({
-            uid: doc.id, // Use the id property of the document as uid
+            uid: doc.id,
             displayName: `${neighborData.firstName} ${neighborData.lastName}`,
             profilePicture: neighborData.profilePicture || null,
             isFriend: !!(neighborData.friends && neighborData.friends[user.uid]),
           });
         }
       });
-      
 
-console.log('Neighbours Data-------',neighborsData);
       setNeighbors(neighborsData);
     } catch (error) {
       console.error('Error fetching neighbors: ', error);
     }
   };
-  
 
   const handleAddRemoveFriend = async (neighbor) => {
     try {
       const user = auth.currentUser;
-      console.log('Current User:', user);
-      
+
       if (!user) {
         console.error('User is not authenticated or currentUser is null.');
         return;
       }
-      
-      const userRef = doc(getFirestore(), 'users', user.uid);
-      console.log('User Reference:', userRef);
-      
-      // Rest of your code...
-      
 
-  
+      const userRef = doc(getFirestore(), 'users', user.uid);
+
       // Check if they are already friends
       if (neighbor.isFriend) {
         // Remove friend relationship
-        await updateDoc(doc(getFirestore(), 'users', user.uid), {
-            [`friends.${neighbor.uid}`]: null,
-          });
-          
-          await updateDoc(doc(getFirestore(), 'users', neighbor.uid), {
-            [`friends.${user.uid}`]: null,
-          });
+        await updateDoc(userRef, { [`friends.${neighbor.uid}`]: null });
+        await updateDoc(doc(getFirestore(), 'users', neighbor.uid), { [`friends.${user.uid}`]: null });
       } else {
         // Add friend relationship
-        await updateDoc(doc(getFirestore(), 'users', user.uid), {
-            [`friends.${neighbor.uid}`]: true,
-          });
-          
-          await updateDoc(doc(getFirestore(), 'users', neighbor.uid), {
-            [`friends.${user.uid}`]: true,
-          });
+        await updateDoc(userRef, { [`friends.${neighbor.uid}`]: true });
+        await updateDoc(doc(getFirestore(), 'users', neighbor.uid), { [`friends.${user.uid}`]: true });
       }
-  
+
       // Refresh the neighbor list
       fetchNeighbors();
     } catch (error) {
       console.error('Error adding/removing friend: ', error);
     }
   };
-  
-  
-  
-  
-  
 
   const handleChatPress = (neighbor) => {
     // Navigate to the chat screen with the selected neighbor
@@ -105,13 +73,10 @@ console.log('Neighbours Data-------',neighborsData);
   };
 
   const renderNeighborItem = ({ item }) => (
-    <TouchableOpacity
-      style={styles.neighborItem}
-      onPress={() => item.isFriend && handleChatPress(item)}>
-
+    <TouchableOpacity style={styles.neighborItem} onPress={() => item.isFriend && handleChatPress(item)}>
       <Image
         source={
-            typeof item.profilePicture === 'string'
+          typeof item.profilePicture === 'string'
             ? { uri: item.profilePicture }
             : typeof item.profilePicture === 'number'
             ? item.profilePicture
@@ -126,15 +91,11 @@ console.log('Neighbours Data-------',neighborsData);
         </Text>
       </View>
       {item.isFriend && (
-      <TouchableOpacity
-        style={styles.chatButton}
-        onPress={() => handleChatPress(item)}>
-        <Text style={styles.chatButtonText}>Chat</Text>
-      </TouchableOpacity>
-    )}
-      <TouchableOpacity
-        style={styles.addRemoveButton}
-        onPress={() => handleAddRemoveFriend(item)}>
+        <TouchableOpacity style={styles.chatButton} onPress={() => handleChatPress(item)}>
+          <Text style={styles.chatButtonText}>Chat</Text>
+        </TouchableOpacity>
+      )}
+      <TouchableOpacity style={styles.addRemoveButton} onPress={() => handleAddRemoveFriend(item)}>
         <Text style={styles.addRemoveButtonText}>
           {item.isFriend ? 'Remove' : 'Add'}
         </Text>
@@ -197,9 +158,11 @@ const styles = StyleSheet.create({
   addRemoveButton: {
     padding: 10,
     borderRadius: 5,
-    backgroundColor: '#0077FF',
+    backgroundColor: '#353453',
     justifyContent: 'center',
     alignItems: 'center',
+    marginLeft: 5
+    
   },
   addRemoveButtonText: {
     color: 'white',
--- screens/TaskChat.js
+++ screens/TaskChat.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useLayoutEffect, useCallback } from 'react';
-import { TouchableOpacity, View, Text, Image } from 'react-native';
+import { TouchableOpacity, View, Text, Image} from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
 import { collection, addDoc, orderBy, where, query, onSnapshot } from 'firebase/firestore';
 import { signOut } from 'firebase/auth';
@@ -10,34 +10,44 @@ import colors from '../colors';
 
 export default function Chat() {
   const [messages, setMessages] = useState([]);
-  const [neighbor, setNeighbor] = useState({}); // State to store neighbor information
+  const [chatParticipants, setChatParticipants] = useState([]); // Array to store participants of the current chat
   const navigation = useNavigation();
   const route = useRoute();
-  const { recipient } = route.params; // Extract recipient from route params
+  const { task } = route.params;
+
+  const onSignOut = () => {
+    signOut(auth).catch((error) => console.log('Error logging out: ', error));
+  };
 
   useLayoutEffect(() => {
-    console.log('Neigjbopr',neighbor);
     navigation.setOptions({
-      headerTitle: () => (
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Image
-            source={{ uri: neighbor.profilePicture }}
-            style={{ width: 40, height: 40, borderRadius: 20, marginRight: 10 }}
-          />
-          <Text style={{ fontSize: 18, color: 'black' }}>{neighbor.displayName}</Text>
+      headerRight: () => (
+        <TouchableOpacity
+          style={{
+            marginRight: 10,
+          }}
+          onPress={onSignOut}
+        >
+          <AntDesign name="logout" size={24} color={colors.gray} style={{ marginRight: 10 }} />
+        </TouchableOpacity>
+      ),
+      headerLeft: () => (
+        <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 10 }}>
+          <Image source={{ uri: task.profilePicture }} style={{ width: 40, height: 40, borderRadius: 20 }} />
+          <Text style={{ fontSize: 16, fontWeight: 'bold', color: 'white', marginLeft: 10 }}>{task.task_description.user_name}</Text>
         </View>
       ),
     });
-  }, [navigation, neighbor]);
+  }, [navigation, task.profilePicture, task.task_description.user_name]);
 
   useLayoutEffect(() => {
     const collectionRef = collection(database, 'chats');
   
-    if (neighbor.uid && auth.currentUser.uid) {
+    if (task.selectedVolunteerId && auth.currentUser.uid) {
       const firstParticipantQuery = query(
         collectionRef,
         orderBy('createdAt', 'desc'),
-        where('participants', 'array-contains', neighbor.uid)
+        where('participants', 'array-contains', task.selectedVolunteerId)
       );
   
       const unsubscribe = onSnapshot(firstParticipantQuery, (querySnapshot) => {
@@ -65,17 +75,11 @@ export default function Chat() {
   
       return unsubscribe;
     }
-  }, [neighbor]);
-  
+  }, [task]);
+
   useEffect(() => {
-    // Set neighbor information when the component mounts
-    console.log("Receii",recipient);
-    setNeighbor({
-      uid: recipient.uid,
-      displayName: recipient.displayName,
-      profilePicture: recipient.profilePicture,
-    });
-  }, []);
+    setChatParticipants([task.selectedVolunteerId, auth.currentUser.uid]);
+  }, [task]);
 
   const onSend = useCallback(
     (messages = []) => {
@@ -91,7 +95,7 @@ export default function Chat() {
       setMessages((previousMessages) => GiftedChat.append(previousMessages, newMessages));
   
       // Include UIDs of both participants in the "participants" field
-      const participants = [auth.currentUser.uid, neighbor.uid];
+      const participants = [auth.currentUser.uid, task.selectedVolunteerId];
   
       addDoc(collection(database, 'chats'), {
         createdAt: new Date(),
@@ -99,10 +103,10 @@ export default function Chat() {
         participants: participants,
       });
     },
-    [auth.currentUser.uid, neighbor.uid]
+    [auth.currentUser.uid, task.selectedVolunteerId]
   );
   
-
+  
 
   return (
     <GiftedChat
@@ -119,8 +123,8 @@ export default function Chat() {
         borderRadius: 20,
       }}
       user={{
-        _id: neighbor.uid, // Use the neighbor's ID
-        avatar: neighbor.profilePicture, // Use the neighbor's profile picture or a default avatar
+        _id: auth.currentUser.uid,
+        avatar: auth.currentUser.photoURL,
       }}
     />
   );
